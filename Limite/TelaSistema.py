d0dceb3e47899d53311f87f958fad6835fc0a67e
Matheus Sebold <matheusssebold@gmail.com>
2025-06-23 19:19:11 -0300

    Start T2

--- Limite/TelaSistema.py
+++ Limite/TelaSistema.py
@@ -1,10 +1,12 @@
 from Limite.Tela import Tela
+import PySimpleGUI as sg
 
 class TelaSistema(Tela):
     def __init__(self, controladorSistema):
         self.__controladorSistema = controladorSistema
 
     def exibirMenuPrincipal(self):
+        '''
         print("\n--- Sistema de Votação do Oscar ---")
         print("1. Membros")
         print("2. Pessoas")
@@ -15,7 +17,28 @@ class TelaSistema(Tela):
         print("7. Votos")
         print("8. Resultados Finais")
         print("0. Sair")
-        return int(input("Escolha: ").strip())
+        '''
+        
+        # Define o layout da janela
+        layout = [[sg.Button('My first Button!'), sg.Button('Disabled Button!', disabled=True)]]
+
+
+        # Cria a janela
+        window = sg.Window("Menu Principal").Layout(layout)
+
+        # Loop de eventos
+        while True:
+            event, values = window.read()
+            
+            if event in (sg.WIN_CLOSED, "Sair"):
+                break
+            else:
+                sg.popup(f"Você clicou em: {event}", title="Ação")
+
+        # Fecha a janela
+        window.close()
+
+        # return int(input("Escolha: ").strip())
 
     def mostrar_resultados(self, resultados):
         print("\n=== RESULTADOS OFICIAIS ===")
